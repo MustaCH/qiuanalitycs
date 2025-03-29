c9c982025b724534bea5cdff13630e6804c797b2
Nacho <polettiignacio7@gmail.com>
2025-03-28 22:04:50 -0300

    FEATURE | Added Beta email's notification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-import node from "@astrojs/node";
-
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
---
+++ src/pages/api/send.ts
@@ -0,0 +1,28 @@
+import type { APIRoute } from "astro";
+import { Resend } from "resend";
+import { render } from "@react-email/render";
+import BetaEmail from "../../emails/betaEmail";
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const GET: APIRoute = async () => {
+
+    const emailContent = BetaEmail({name: 'Ignacio', lastName: 'Poletti'});
+    const emailHtml = await render(emailContent);
+    const emailText = await render(emailContent, { plainText: true });
+  
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["polettiignacio7@gmail.com"],
+      subject: "Bienvenido a la Beta de Qiu",
+      html: emailHtml,
+      text: emailText,
+    });
+
+    if (error) {
+        console.error(error);
+        return new Response(JSON.stringify({success: false, error: error.message}));
+    }
+
+    return new Response(JSON.stringify({data}));
+}
