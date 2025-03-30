d2629697b645f9ec967efd14d2720b3315c02173
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-03-30 13:10:32 -0300

    FETURE | Added new email address and favicon

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -6,23 +6,24 @@ import BetaEmail from "../../emails/betaEmail";
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const GET: APIRoute = async () => {
+  const emailContent = BetaEmail({ name: "Ignacio", lastName: "Poletti" });
+  const emailHtml = await render(emailContent);
+  const emailText = await render(emailContent, { plainText: true });
 
-    const emailContent = BetaEmail({name: 'Ignacio', lastName: 'Poletti'});
-    const emailHtml = await render(emailContent);
-    const emailText = await render(emailContent, { plainText: true });
-  
-    const { data, error } = await resend.emails.send({
-      from: "Qiu <onboarding@resend.dev>",
-      to: ["polettiignacio7@gmail.com"],
-      subject: "Bienvenido a la Beta de Qiu",
-      html: emailHtml,
-      text: emailText,
-    });
+  const { data, error } = await resend.emails.send({
+    from: "Qiu <team@qiuanalitycs.com>",
+    to: ["polettiignacio7@gmail.com"],
+    subject: "Bienvenido a la Beta de Qiu",
+    html: emailHtml,
+    text: emailText,
+  });
 
-    if (error) {
-        console.error(error);
-        return new Response(JSON.stringify({success: false, error: error.message}));
-    }
+  if (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ success: false, error: error.message })
+    );
+  }
 
-    return new Response(JSON.stringify({data}));
-}
+  return new Response(JSON.stringify({ data }));
+};
