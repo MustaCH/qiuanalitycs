c07e6c786c87fb756a8f718e2c32ec1bd6ee1d06
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-03-31 01:51:30 -0300

    FEATURE | Actualización de logos y archivos SVG

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,31 +7,31 @@ import { z } from "astro:schema";
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const server = {
-    send: defineAction({
-        accept: "form",
-        input: z.object({
-            name: z.string(),
-            lastName: z.string(),
-            email: z.string().email(),
-        }),
-        handler: async ({name, lastName, email}) => {
-            const emailContent = BetaEmail({name: name, lastName: lastName});
-            const emailHtml = await render(emailContent);
-            const emailText = await render(emailContent, { plainText: true });
-  
-            const { data, error } = await resend.emails.send({
-                from: "Qiu <onboarding@resend.dev>",
-                to: [email],
-                subject: `Bienvenido ${name} a la Beta de Qiu`,
-                html: emailHtml,
-                text: emailText,
-            });
-
-            if (error) {
-                throw error;
-            }
-
-            return data;
-        },
+  send: defineAction({
+    accept: "form",
+    input: z.object({
+      name: z.string().min(2, "Nombre requerido"),
+      lastName: z.string().min(2, "Apellido requerido"),
+      email: z.string().email("Email inválido"),
     }),
-}
+    handler: async ({ name, lastName, email }) => {
+      const emailContent = BetaEmail({ name: name, lastName: lastName });
+      const emailHtml = await render(emailContent);
+      const emailText = await render(emailContent, { plainText: true });
+
+      const { data, error } = await resend.emails.send({
+        from: "Qiu <team@qiuanalitycs.com>",
+        to: [email],
+        subject: `Bienvenido ${name} a la Beta de Qiu`,
+        html: emailHtml,
+        text: emailText,
+      });
+
+      if (error) {
+        throw error;
+      }
+
+      return data;
+    },
+  }),
+};
